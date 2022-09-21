432811f770a8da1db71b175c440725446d0beed3
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-09-21 09:21:38 -0300

    Project Finished

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,37 @@
 import { useParams } from "react-router-dom";
-import { Container, Owner, Loading, BackButton } from "./styles";
-import { FaArrowLeft } from "react-icons/fa";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+  FilterList,
+} from "./styles";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+
+type Issues = {
+  id: number;
+  user: any;
+  html_url: any;
+  title: any;
+  labels: any;
+};
 
 export default function Repository() {
   const { repositorio } = useParams();
   const [repo, setRepo] = useState<any>({});
-  const [issues, setIssues] = useState([]);
+  const [issues, setIssues] = useState<Issues[]>([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState([
+    { state: "all", label: "Todas", active: true },
+    { state: "open", label: "Abertas", active: false },
+    { state: "closed", label: "Fechadas", active: false },
+  ]);
+  const [filterIndex, setFilterIndex] = useState(0);
 
   useEffect(() => {
     async function load() {
@@ -16,19 +39,41 @@ export default function Repository() {
         api.get(`/repos/${repositorio}`),
         api.get(`/repos/${repositorio}/issues`, {
           params: {
-            state: "open",
+            state: filter.find((f) => f.active)?.state,
             per_page: 5,
           },
         }),
       ]);
       setRepo(repositorioData.data);
-      console.log(repo);
       setIssues(issues.data);
       setLoading(false);
     }
 
     load();
-  }, [repositorio]);
+  }, []);
+
+  useEffect(() => {
+    async function loadIssue() {
+      const response = await api.get(`/repos/${repositorio}/issues`, {
+        params: {
+          state: filter[filterIndex].state,
+          page,
+          per_page: 5,
+        },
+      });
+
+      setIssues(response.data);
+    }
+    loadIssue();
+  }, [page, filterIndex, filter]);
+
+  function handlePage(action: string) {
+    setPage(action === "back" ? page - 1 : page + 1);
+  }
+
+  function handleFilter(index: number) {
+    setFilterIndex(index);
+  }
 
   if (loading) {
     return (
@@ -47,6 +92,47 @@ export default function Repository() {
           <h1>{repo.name}</h1>
           <p>{repo.description}</p>
         </Owner>
+        <FilterList active={filterIndex}>
+          {filter.map((filter, index) => (
+            <button
+              type="button"
+              key={filter.label}
+              onClick={() => handleFilter(index)}
+            >
+              {filter.label}
+            </button>
+          ))}
+        </FilterList>
+        <IssuesList>
+          {issues.map((issue) => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+
+                  {issue.labels.map((label: { id: number; name: string }) => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
+        <PageActions>
+          <button
+            type="button"
+            onClick={() => handlePage("back")}
+            disabled={page < 2}
+          >
+            <FaArrowLeft color="#fff" size={18} />
+          </button>
+          <button type="button" onClick={() => handlePage("next")}>
+            <FaArrowRight color="#fff" size={18} />
+          </button>
+        </PageActions>
       </Container>
     );
   }
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+type Props = {
+    active: number
+}
+
+
 export const Loading = styled.div`
 color: #fff;
 display: flex;
@@ -44,4 +49,91 @@ export const BackButton = styled(Link)`
 border: 0;
 outline: 0;
 background: transparent;
+`
+
+export const IssuesList = styled.ul`
+margin-top: 30px;
+padding-top: 30px;
+border-top: 1px solid #eee;
+list-style: none;
+
+li{
+    display: flex;
+    padding: 15px 10px;
+
+    img{
+        width: 36px;
+        height: 36px;
+        border-radius: 50%;
+        border: 2px solid #0D2636;
+    }
+    div{
+        flex:1;
+        margin-left: 12px;
+     p{
+        margin-top: 10px;
+        font-size: 12px;
+        color: #000;
+    }
+    }
+    strong{
+        font-size: 15px;
+    a{
+        text-decoration: none;
+        color:#222;
+        transform: 0.3s;
+        &:hover{
+            color: #0071db;
+        }
+    }
+
+    span{
+        background:#222;
+        color: #FFF;
+        border-radius: 4px;
+        font-size: 12px;
+        font-weight: 600;
+        padding: 4px 7px;
+        margin-left: 10px;
+    }
+}
+}
+`
+
+export const PageActions = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+margin-top: 1rem;
+
+button{
+    outline: 0;
+    border: 0;
+    background: #222;
+    color: #fff;
+    padding: 10px;
+    border-radius: 50%;
+    transition: 0.5s;
+    &:disabled{
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+}
+`
+
+export const FilterList = styled.div<Props>`
+margin: 15px 0;
+button{
+    outline: 0;
+    border: 0;
+    padding: 8px;
+    border-radius: 4px;
+    margin: 0 3px;
+
+    &:nth-child(${(props) => props.active + 1}){
+        background: #0071db;
+        color: #fff;
+    }
+
+}
 `
